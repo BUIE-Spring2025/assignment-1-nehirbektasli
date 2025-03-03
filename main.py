a6349bf3474ae8c561b20c6454f970c7d2c5981b
Nehir Bektaşlı <nehir.bektasli@std.bogazici.edu.tr>
2025-03-03 19:22:19 +0300

    got rid of the error for when there is an invalid input (by breaking the for loop when the constraint is not satisfied). Also, made the code cleaner by defining x.

--- main.py
+++ main.py
@@ -5,18 +5,20 @@ def int_to_roman(num):
     :param num: Integer value between 1 and 3999 inclusive.
     :return: A string representing the Roman numeral of the integer.
     """
-    if not 1 <= num <= 3999:
-        print("Input must be an integer from 1 to 3999. Please enter a valid input.")
     roman_num = ""
     decimal = {1000: "M", 500: "D", 100: "C", 50: "L", 10: "X", 5: "V", 1: "I"}
     for i in [1000, 100, 10, 1]:
-        if num // i == 0:
+        if not 1 <= num <= 3999:
+            print("Invalid input. Please enter an integer between 1 and 3999.")
+            break
+        x = num // i
+        if x == 0:
             continue
-        elif num // i == 9:
-            roman_num += decimal[i] + decimal[10*i]
-        elif num // i == 4:
-            roman_num += decimal[i] + decimal[5*i]
+        elif x == 9 or x == 4:
+            roman_num += decimal[i] + decimal[(x + 1) * i]
+        elif x < 4:
+            roman_num += x * decimal[i]
         else:
-            roman_num += (num // i) * decimal[i]
+            roman_num += decimal[5*i] + (x - 5) * decimal[i]
         num = num % i
     return roman_num
